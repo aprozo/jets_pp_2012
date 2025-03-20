b291b074fc52ea22c674f42f3ef87ccf59f4d129
aprozo <alexandr.a.prozorov@gmail.com>
2025-03-20 14:20:46 +0100

    added python style notebook

---
+++ macros/ana/ana_trees_simple.cxx
@@ -0,0 +1,278 @@
+
+#include <iostream>
+
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TLegend.h"
+#include "TString.h"
+#include "TTree.h"
+
+#include "TStyle.h"
+
+using namespace std;
+
+//! ----------------------------------------------------
+struct InputTreeEntry {
+  int runid;
+  int runid1;
+  int eventid;
+  double weight;
+  double refmult;
+  int njets;
+  double vz;
+  int mult;
+  float event_sum_pt;
+  bool is_rejected;
+  double neutral_fraction[1000];
+  bool trigger_match[1000];
+  double pt[1000];
+  int n_constituents[1000];
+  int index[1000];
+};
+void checkExistence(TFile *file, TString name) {
+  if (!file->Get(name)) {
+    cout << "Error: " << name << " not found!" << endl;
+    exit(1);
+  }
+}
+
+int ana_trees_simple(TString OutFile = "hists2.root") {
+
+  gStyle->SetOptStat(0);
+  float RCut = 0.6;
+  float EtaCut = 1.0 - RCut;
+
+  //================================================================================================
+
+  TFile *eventsFile = new TFile("events.root", "READ");
+  // TH1D *weights = (TH1D *)eventsFile->Get("hist_JP2_weight");
+  TH1D *nEvents_MB = (TH1D *)eventsFile->Get("hEventsRun_MB");
+
+  TH1D *nEvents_JP2 = (TH1D *)eventsFile->Get("hEventsRun_JP2");
+  TH1D *hist_sampled_lumi_VPDMB =
+      (TH1D *)eventsFile->Get("hist_sampled_lumi_VPDMB");
+  TH1D *hist_prescale_VPDMB = (TH1D *)eventsFile->Get("hist_prescale_VPDMB");
+  TH1D *hist_livetime_VPDMB = (TH1D *)eventsFile->Get("hist_livetime_VPDMB");
+  TH1D *hist_max_nevents_VPDMB =
+      (TH1D *)eventsFile->Get("hist_max_nevents_VPDMB");
+  TH1D *hist_sampled_lumi_JP2 =
+      (TH1D *)eventsFile->Get("hist_sampled_lumi_JP2");
+  TH1D *hist_prescale_JP2 = (TH1D *)eventsFile->Get("hist_prescale_JP2");
+  TH1D *hist_livetime_JP2 = (TH1D *)eventsFile->Get("hist_livetime_JP2");
+  TH1D *hist_max_nevents_JP2 = (TH1D *)eventsFile->Get("hist_max_nevents_JP2");
+  TH1D *hist_JP2_weight = (TH1D *)eventsFile->Get("hist_JP2_weight");
+
+  //================================================================================================
+
+  TFile *outputFile = new TFile(OutFile, "RECREATE");
+  TH1D *JP2 =
+      new TH1D("JP2", "; p_{T} (GeV/c); Entries (per event)", 100, 0, 100);
+  TH1D *JP2_Livetime = (TH1D *)JP2->Clone("JP2_Livetime");
+  TH1D *JP2_Prescale = (TH1D *)JP2->Clone("JP2_Prescale");
+  TH1D *JP2_SampledLumi = (TH1D *)JP2->Clone("JP2_SampledLumi");
+  TH1D *JP2_MaxNEvents = (TH1D *)JP2->Clone("JP2_MaxNEvents");
+  TH1D *JP2_MatchTrigger = (TH1D *)JP2->Clone("JP2_MatchTrigger");
+
+  int nRunBins = nEvents_JP2->GetNbinsX();
+  TH2D *JP2_run = new TH2D("JP2_run", "; p_{T} (GeV/c); Entries (per event)",
+                           100, 0, 100, nRunBins, 0, nRunBins);
+
+  vector<TString> runBins;
+  // set names of bins to run numbers
+  for (unsigned int i = 1; i <= nRunBins; ++i) {
+    TString runBin = nEvents_JP2->GetXaxis()->GetBinLabel(i);
+    runBins.push_back(runBin);
+    JP2_run->GetYaxis()->SetBinLabel(i, runBin);
+  }
+  TH2D *JP2_run_Livetime = (TH2D *)JP2_run->Clone("JP2_run_Livetime");
+  TH2D *JP2_run_Prescale = (TH2D *)JP2_run->Clone("JP2_run_Prescale");
+  TH2D *JP2_run_SampledLumi = (TH2D *)JP2_run->Clone("JP2_run_SampledLumi");
+  TH2D *JP2_run_MaxNEvents = (TH2D *)JP2_run->Clone("JP2_run_MaxNEvents");
+  TH2D *JP2_run_MatchTrigger = (TH2D *)JP2_run->Clone("JP2_run_MatchTrigger");
+
+  //================================================================================================
+
+  TFile *mb_hist_file = new TFile("hists_mb.root", "READ");
+  // TFile *mb_hist_file = new TFile("hists.root", "READ");
+  TH1D *hPtMb = (TH1D *)mb_hist_file->Get("hPtMb");
+
+  TFile *jp2_jets = new TFile("../tree_jets.root", "READ");
+  if (!jp2_jets || jp2_jets->IsZombie()) {
+    cout << "Error: JP2 tree file not found!" << endl;
+    return -1;
+  }
+  TTree *jp2_tree = (TTree *)jp2_jets->Get("ResultTree");
+
+  InputTreeEntry jp2;
+  jp2_tree->SetBranchAddress("runid1", &jp2.runid1);
+  jp2_tree->SetBranchAddress("vz", &jp2.vz);
+  jp2_tree->SetBranchAddress("neutral_fraction", jp2.neutral_fraction);
+  jp2_tree->SetBranchAddress("trigger_match_jp", jp2.trigger_match);
+  jp2_tree->SetBranchAddress("pt", jp2.pt);
+  jp2_tree->SetBranchAddress("njets", &jp2.njets);
+
+  for (int i = 0; i < jp2_tree->GetEntries(); ++i) {
+    jp2_tree->GetEntry(i);
+    //   show progress
+    if (i % 100000 == 0) {
+      cout << "Processing JP2 entry: " << i << " / " << jp2_tree->GetEntries()
+           << endl;
+    }
+    if (jp2.njets < 1)
+      continue; // skip events with no jets
+    if (jp2.vz < -30 || jp2.vz > 30)
+      continue; // vz cut
+    int run = jp2.runid1;
+    if (run == 13049007 || run == 13048092 || run == 13049006 ||
+        run == 13051099 || run == 13051074 || run == 13064067 ||
+        run == 13070061 || run == 13069023 || run == 13048093 ||
+        run == 13068060 || run == 13052061 || run == 13048019 ||
+        run == 13061035) // Livetime JP2 very poor!!!!
+      continue;
+
+    TString runstring = Form("%i", run);
+    int binRun = nEvents_JP2->GetXaxis()->FindBin(Form("%i", run));
+
+    double events_in_run = nEvents_JP2->GetBinContent(binRun);
+
+    double weight_prescale = hist_prescale_VPDMB->GetBinContent(binRun) /
+                             hist_prescale_JP2->GetBinContent(binRun);
+
+    double weight_livetime = hist_livetime_JP2->GetBinContent(binRun) /
+                             hist_livetime_VPDMB->GetBinContent(binRun);
+
+    double weight_max_nevents = hist_max_nevents_VPDMB->GetBinContent(binRun) /
+                                hist_max_nevents_JP2->GetBinContent(binRun);
+
+    double total_weight = events_in_run;
+    // cout << "======================================" << endl;
+
+    // fill the histogram with the event weight
+    for (int j = 0; j < jp2.njets; ++j) {
+      total_weight *= 1;
+      JP2->Fill(jp2.pt[j], 1. / total_weight);
+      JP2_run->Fill(jp2.pt[j], runstring, 1. / total_weight);
+
+      total_weight *= weight_livetime;
+      JP2_Livetime->Fill(jp2.pt[j], 1. / total_weight);
+      JP2_run_Livetime->Fill(jp2.pt[j], runstring, 1. / total_weight);
+
+      total_weight *= weight_max_nevents;
+      JP2_MaxNEvents->Fill(jp2.pt[j], 1. / total_weight);
+      JP2_run_MaxNEvents->Fill(jp2.pt[j], runstring, 1. / total_weight);
+
+      total_weight *= weight_prescale;
+      JP2_Prescale->Fill(jp2.pt[j], 1. / total_weight);
+      JP2_run_Prescale->Fill(jp2.pt[j], runstring, 1. / total_weight);
+
+      if (jp2.trigger_match[j]) {
+        JP2_MatchTrigger->Fill(jp2.pt[j], 1. / total_weight);
+        JP2_run_MatchTrigger->Fill(jp2.pt[j], runstring, 1. / total_weight);
+      }
+    }
+  }
+
+  TCanvas *can = new TCanvas("can", "can", 800, 600);
+  can->cd();
+  TLegend *leg = new TLegend(0.6, 0.7, 0.9, 0.9);
+  leg->AddEntry(hPtMb, "MB", "l");
+  leg->AddEntry(JP2, "JP2", "pel");
+  leg->AddEntry(JP2_Livetime, "+Livetime", "l");
+  leg->AddEntry(JP2_Prescale, "+Livetime+Prescale", "l");
+  leg->AddEntry(JP2_MaxNEvents, "+Livetime+Prescale+MaxEvents", "l");
+  leg->AddEntry(JP2_MatchTrigger, "+Livetime+Prescale+MaxEvents+MatchTrigger",
+                "l");
+  JP2->SetLineColor(2009);
+  JP2_Livetime->SetLineColor(2001);
+  JP2_Prescale->SetLineColor(2002);
+  JP2_MaxNEvents->SetLineColor(2003);
+  hPtMb->SetLineColor(2004);
+  JP2_MatchTrigger->SetLineColor(2006);
+
+  gPad->SetLogy();
+  JP2->GetYaxis()->SetRangeUser(1e-8, 1e3);
+  JP2->Draw("pe ");
+
+  hPtMb->Draw("same hist");
+  leg->Draw("same hist");
+  JP2_Livetime->Draw("same hist");
+  JP2_MaxNEvents->Draw("same hist");
+  JP2_Prescale->Draw("same hist");
+  JP2_MatchTrigger->Draw("same hist");
+  can->SaveAs("JP2.pdf");
+
+  TCanvas *runRatio = new TCanvas("runRatio", "runRatio", 800, 600);
+  runRatio->cd();
+  gPad->SetLogy();
+  TH1D *weight_prescale_run =
+      (TH1D *)hist_prescale_VPDMB->Clone("weight_prescale_run");
+  weight_prescale_run->Divide(hist_prescale_JP2);
+  weight_prescale_run->SetLineColor(2002);
+  weight_prescale_run->GetYaxis()->SetRangeUser(0.5, 1e3);
+  weight_prescale_run->Draw();
+
+  TH1D *weight_livetime_run =
+      (TH1D *)hist_livetime_JP2->Clone("weight_livetime_run");
+  weight_livetime_run->Divide(hist_livetime_VPDMB);
+  weight_livetime_run->SetLineColor(2001);
+  weight_livetime_run->Draw("same");
+
+  TH1D *weight_max_nevents_run =
+      (TH1D *)hist_max_nevents_VPDMB->Clone("weight_max_nevents_run");
+  weight_max_nevents_run->Divide(hist_max_nevents_JP2);
+  weight_max_nevents_run->SetLineColor(2003);
+  weight_max_nevents_run->Draw("same");
+
+  // Ratios to VPD MB
+  TH1D *JP2_Livetime_Ratio = (TH1D *)JP2_Livetime->Clone("JP2_Livetime_Ratio");
+  JP2_Livetime_Ratio->Divide(hPtMb);
+  TH1D *JP2_Prescale_Ratio = (TH1D *)JP2_Prescale->Clone("JP2_Prescale_Ratio");
+  JP2_Prescale_Ratio->Divide(hPtMb);
+  TH1D *JP2_MaxNEvents_Ratio =
+      (TH1D *)JP2_MaxNEvents->Clone("JP2_MaxNEvents_Ratio");
+  JP2_MaxNEvents_Ratio->Divide(hPtMb);
+
+  TH1D *JP2_TrigMatch_Ratio =
+      (TH1D *)JP2_MatchTrigger->Clone("JP2_TrigMatch_Ratio");
+  JP2_TrigMatch_Ratio->Divide(hPtMb);
+
+  TLegend *leg2 = new TLegend(0.6, 0.7, 0.9, 0.9);
+  leg2->AddEntry(weight_prescale_run, "Prescale MB/JP2", "l");
+  leg2->AddEntry(weight_livetime_run, "Livetime JP2/MB", "l");
+  leg2->AddEntry(weight_max_nevents_run, "MaxNEvents MB/JP2", "l");
+  leg2->Draw();
+
+  // can->cd();
+  // JP2_Livetime_Ratio->Draw();
+  // JP2_Prescale_Ratio->Draw("same hist");
+  // JP2_MaxNEvents_Ratio->Draw("same hist");
+  // leg->Draw();
+  // can->SaveAs("Ratio.pdf");
+
+  // Write histograms to the output file
+  outputFile->cd();
+  JP2->Write();
+  hPtMb->Write();
+  JP2_Livetime->Write();
+  JP2_Prescale->Write();
+  JP2_MaxNEvents->Write();
+  JP2_MatchTrigger->Write();
+  JP2_run->Write();
+  JP2_run_Livetime->Write();
+  JP2_run_Prescale->Write();
+  JP2_run_MaxNEvents->Write();
+  JP2_run_MatchTrigger->Write();
+
+  can->Write();
+
+  JP2_Livetime_Ratio->Write();
+  JP2_Prescale_Ratio->Write();
+  JP2_MaxNEvents_Ratio->Write();
+  JP2_TrigMatch_Ratio->Write();
+
+  // outputFile->Close();
+
+  return 0;
+}
---
+++ macros/ana/get_run_events.cxx
@@ -0,0 +1,195 @@
+#include <TFile.h>
+#include <TH1.h>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+
+// Define a struct to hold all the run information
+struct RunInfo {
+  double sampled_lumi_VPDMB;
+  double prescale_VPDMB;
+  double livetime_VPDMB;
+  int max_nevents_VPDMB;
+  double sampled_lumi_JP2;
+  double prescale_JP2;
+  double livetime_JP2;
+  int max_nevents_JP2;
+  double JP2_weight;
+};
+
+std::map<int, RunInfo> CreateRunInfoMap(
+    const char *filename = "../lists/files_lumi_jaime/lumi_VPDMB_JP2.csv") {
+  // Open the file
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    std::cerr << "Error: Could not open file " << filename << std::endl;
+  }
+
+  std::map<int, RunInfo> runInfoMap;
+
+  // Read the header line and discard
+  std::string header;
+  std::getline(file, header);
+
+  // Read data line by line
+  std::string line;
+  while (std::getline(file, line)) {
+    try {
+      std::stringstream ss(line);
+      std::string token;
+
+      // Extract run number
+      std::getline(ss, token, ',');
+      int run = std::stoi(token);
+
+      // Create a new RunInfo object
+      RunInfo info;
+
+      // Parse remaining columns
+      std::getline(ss, token, ',');
+      info.sampled_lumi_VPDMB = std::stod(token);
+
+      std::getline(ss, token, ',');
+      info.prescale_VPDMB = std::stod(token);
+
+      std::getline(ss, token, ',');
+      info.livetime_VPDMB = std::stod(token);
+
+      std::getline(ss, token, ',');
+      info.max_nevents_VPDMB = std::stoi(token);
+
+      std::getline(ss, token, ',');
+      info.sampled_lumi_JP2 = std::stod(token);
+
+      std::getline(ss, token, ',');
+      info.prescale_JP2 = std::stod(token);
+
+      std::getline(ss, token, ',');
+      info.livetime_JP2 = std::stod(token);
+
+      std::getline(ss, token, ',');
+      info.max_nevents_JP2 = std::stoi(token);
+
+      std::getline(ss, token, ',');
+      info.JP2_weight = std::stod(token);    // JP2_weight = (prescale_VPDMB/prescale_JP2)*(livetime_JP2/livetime_VPDMB)*(max_nevents_VPDMB/max_nevents_JP2)
+
+      // Add to map
+      runInfoMap[run] = info;
+    } catch (const std::exception &e) {
+      std::cerr << "Error parsing line: " << line << std::endl;
+      std::cerr << "Exception: " << e.what() << std::endl;
+      // Continue to next line
+      continue;
+    }
+  }
+  // Close the file
+  file.close();
+
+  return runInfoMap;
+}
+
+int get_run_events() {
+
+  // Create a map to store run number and associated information
+  std::map<int, RunInfo> runInfoMap =
+      CreateRunInfoMap("../lists/files_lumi_james_dunlop/lumi_VPDMB_JP2.csv");
+
+  TString mb_file_name = "../output/MB/tree_jets.root";
+  TFile *mb_file = new TFile(mb_file_name, "READ");
+
+  TString jp_file_name = "../output/JP2/tree_jets.root";
+  TFile *jp_file = new TFile(jp_file_name, "READ");
+
+  TH1D *hEventsRun_MB = (TH1D *)mb_file->Get("hEventsRun");
+  TH1D *hEventsRun_JP2 = (TH1D *)jp_file->Get("hEventsRun");
+
+  hEventsRun_MB->SetName("hEventsRun_MB");
+  hEventsRun_JP2->SetName("hEventsRun_JP2");
+
+  TH1D *hRatio_MB_JP2 = (TH1D *)hEventsRun_MB->Clone("hRatio_MB_JP2");
+  hRatio_MB_JP2->Divide(hEventsRun_JP2);
+  hRatio_MB_JP2->SetTitle("Ratio of MB to JP2 events; Run Number; #frac{MB}{JP2}");
+
+  TH1D *temp = (TH1D *)hRatio_MB_JP2->Clone("temp");
+  temp->SetTitle(";Run Number;");
+  // clear the bin content and errors
+  for (int i = 1; i <= temp->GetNbinsX(); ++i) {
+    temp->SetBinContent(i, 0);
+    temp->SetBinError(i, 0);
+  }
+  TH1D *hist_sampled_lumi_VPDMB =
+      (TH1D *)temp->Clone("hist_sampled_lumi_VPDMB");
+  TH1D *hist_prescale_VPDMB = (TH1D *)temp->Clone("hist_prescale_VPDMB");
+  TH1D *hist_livetime_VPDMB = (TH1D *)temp->Clone("hist_livetime_VPDMB");
+  TH1D *hist_max_nevents_VPDMB = (TH1D *)temp->Clone("hist_max_nevents_VPDMB");
+  TH1D *hist_sampled_lumi_JP2 = (TH1D *)temp->Clone("hist_sampled_lumi_JP2");
+  TH1D *hist_prescale_JP2 = (TH1D *)temp->Clone("hist_prescale_JP2");
+  TH1D *hist_livetime_JP2 = (TH1D *)temp->Clone("hist_livetime_JP2");
+  TH1D *hist_max_nevents_JP2 = (TH1D *)temp->Clone("hist_max_nevents_JP2");
+  TH1D *hist_JP2_weight = (TH1D *)temp->Clone("hist_JP2_weight");
+
+  // Fill the histograms with the corresponding values from the map
+  for (const auto &entry : runInfoMap) {
+    int run = entry.first;
+    const RunInfo &info = entry.second;
+    int bin = hist_sampled_lumi_VPDMB->GetXaxis()->FindBin(Form("%i", run));
+
+    hist_sampled_lumi_VPDMB->SetBinContent(bin, info.sampled_lumi_VPDMB);
+    hist_prescale_VPDMB->SetBinContent(bin, info.prescale_VPDMB);
+    hist_livetime_VPDMB->SetBinContent(bin, info.livetime_VPDMB);
+    hist_max_nevents_VPDMB->SetBinContent(bin, info.max_nevents_VPDMB);
+    hist_sampled_lumi_JP2->SetBinContent(bin, info.sampled_lumi_JP2);
+    hist_prescale_JP2->SetBinContent(bin, info.prescale_JP2);
+    hist_livetime_JP2->SetBinContent(bin, info.livetime_JP2);
+    hist_max_nevents_JP2->SetBinContent(bin, info.max_nevents_JP2);
+    hist_JP2_weight->SetBinContent(bin, info.JP2_weight);
+
+    // JP2_weight = (prescale_VPDMB/prescale_JP2)*(livetime_JP2/livetime_VPDMB)*(max_nevents_VPDMB/max_nevents_JP2)
+  }
+
+
+  hist_sampled_lumi_VPDMB->SetTitle("Sampled Luminosity VPDMB; Run Number; sampled_lumi_VPDMB (nb^{-1})");
+    hist_prescale_VPDMB->SetTitle("Prescale VPDMB; Run Number; prescale_VPDMB");
+    hist_livetime_VPDMB->SetTitle("Livetime VPDMB; Run Number; livetime_VPDMB");
+    hist_max_nevents_VPDMB->SetTitle("Max Events VPDMB; Run Number; max_nevents_VPDMB");
+    hist_sampled_lumi_JP2->SetTitle("Sampled Luminosity JP2; Run Number; sampled_lumi_JP2 (nb^{-1})");
+    hist_prescale_JP2->SetTitle("Prescale JP2; Run Number; prescale_JP2");
+    hist_livetime_JP2->SetTitle("Livetime JP2; Run Number; livetime_JP2");
+    hist_max_nevents_JP2->SetTitle("Max Events JP2; Run Number; max_nevents_JP2");
+
+
+
+  hist_JP2_weight->SetTitle("#frac{PS_{MB}}/{PS_{JP2}} #frac{livetime_{JP2}}/{livetime_{MB}} #frac{nevents_{MB}}/{nevents_{JP2}}; Run Number; (prescale_MB/prescale_JP2)*(livetime_JP2/livetime_MB)*(max_nevents_MB/max_nevents_JP2)");
+
+  //   self-consistency check on max nevents
+  TH1D *ratio_max_nevents_VPDMB =
+      (TH1D *)hEventsRun_MB->Clone("ratio_max_nevents_VPDMB");
+  ratio_max_nevents_VPDMB->Divide(hist_max_nevents_VPDMB);
+
+  TH1D *ratio_max_nevents_JP2 =
+      (TH1D *)hEventsRun_JP2->Clone("ratio_max_nevents_JP2");
+  ratio_max_nevents_JP2->Divide(hist_max_nevents_JP2);
+
+
+  TFile *fout = new TFile("events.root", "RECREATE");
+  fout->cd();
+  hEventsRun_MB->Write();
+  hEventsRun_JP2->Write();
+  hRatio_MB_JP2->Write();
+  hist_sampled_lumi_VPDMB->Write();
+  hist_prescale_VPDMB->Write();
+  hist_max_nevents_VPDMB->Write();
+  hist_livetime_VPDMB->Write();
+  hist_sampled_lumi_JP2->Write();
+  hist_prescale_JP2->Write();
+  hist_livetime_JP2->Write();
+  hist_max_nevents_JP2->Write();
+  hist_JP2_weight->Write();
+  ratio_max_nevents_VPDMB->Write();
+  ratio_max_nevents_JP2->Write();
+
+  fout->Close();
+
+  return 0;
+}
